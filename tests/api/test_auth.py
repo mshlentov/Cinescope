00c3a5971fe7246e11180ec2398b32ff59f8d4ca
LISTER <mshlentov@yandex.ru>
2025-06-11 15:17:01 +0300

    Добавление позитивных проверок авторизации

--- tests/api/test_auth.py
+++ tests/api/test_auth.py
@@ -1,6 +1,6 @@
 import pytest
 import requests
-from constants import BASE_URL, HEADERS, REGISTER_ENDPOINT
+from constants import BASE_URL, HEADERS, REGISTER_ENDPOINT, LOGIN_ENDPOINT
 
 
 class TestAuthAPI:
@@ -24,4 +24,32 @@ class TestAuthAPI:
 
         # Проверяем, что роль USER назначена по умолчанию
         assert "USER" in response_data["roles"], "Роль USER должна быть у пользователя"
+
+    def test_login_user(self, test_user):
+        # URL для авторизации
+        login_url = f"{BASE_URL}{LOGIN_ENDPOINT}"
+
+        login_data = {
+            "email": test_user["email"],
+            "password": test_user["password"]
+        }
+
+        # Отправка запроса на авторизацию
+        response = requests.post(login_url, json=login_data, headers=HEADERS)
+
+        # Логируем ответ для диагностики
+        print(f"Response status: {response.status_code}")
+        print(f"Response body: {response.text}")
+
+        # Проверки
+        assert response.status_code == 200, "Ошибка авторизации"
+        response_data = response.json()
+        assert "accessToken" in response_data, "Токен отсутствует в ответе"
+        assert "user" in response_data, "Поле 'user' отсутствует в ответе"
+        user_data = response_data["user"]
+        assert "email" in user_data, "Поле 'email' отсутствует в объекте 'user'"
+        assert user_data["email"] == login_data["email"], f"Email не совпадает. Ожидалось: '{login_data["email"]}', Получено: '{user_data['email']}'"
+
+
+
 
